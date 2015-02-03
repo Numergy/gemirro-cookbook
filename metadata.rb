cdae81952016ac92433d7eac4b36266924514a2e
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2015-02-03 17:01:22 +0100

    Bump to 0.3.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'pierre.rambaud@numergy.com'
 license 'Apache 2.0'
 description 'Installs/Configures gemirro'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.3.0'
+version '0.3.1'
 
 depends 'apt'
 depends 'nginx'
